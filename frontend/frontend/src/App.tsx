931533c79532029f3490c9aeebb007c8a7c779a1
Samir Rahman <samir.rahman123@gmail.com>
2024-04-20 16:04:23 -0400

    fixedLogo

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 
 import './App.css';
-import logo from './logo.svg';
+
 
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from "./Home/Home";
